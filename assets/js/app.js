56c5703b4bcbcb223d320b52ce4e375cc4821dd1
ebamberger1 <89244856+ebamberger1@users.noreply.github.com>
2021-08-20 16:08:55 -0500

    Add button and message functionality to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,13 +9,14 @@ var scavengerHunt = {
 			'Many say that their college experiences stem from here. Leave the pie behind, but there is still a crunch to come. Don’t spiral out of control, 3 is prime. A gathering place, a helping hand, your resource for a problem yet unsolved is where you shall go. Where am I?'],
 	// List of keywords used to solve clues
 	keywords: ['stained glass', 'broken windows', 'guatemala', 'admissions'],
-	// Determines the player's starting index and displays the first clue
+	// Determines the player's starting index, displays the first clue, and hides the "Next Clue" button
 	initialize: function(){
 		this.gpPlayer.setStartIdx();
 		showClue(this.clues[this.gpPlayer.getStartIdx()]);
+		document.querySelector("#nextdiv button").disabled = true;
 	},
-	/* Processes the player's guess and determines if it solves the clue. If the clue was solved, the next clue is displayed. Otherwise, a message
-	 * is displayed informing the user that the guess was incorrect.
+	/* Processes the player's guess and determines if it solves the clue. If the clue was solved, a congratulatory message is displayed. 
+	 * Otherwise, a message is displayed informing the user that the guess was incorrect.
 	 * @param guess is a String
 	 */
 	makeGuess: function(guess){
@@ -24,18 +25,31 @@ var scavengerHunt = {
 		if (this.keywords[(startidx + numright) % 4] != guess)
 			showMessage("Try again");
 		else{
-			clearInput();
-			if (numright + 1 == this.keywords.length){
-				showClue(this.clues[this.clues.length - 1]);
-				document.querySelector("#guessdiv input").style.display = "none";
-				document.querySelector("#guessdiv button").style.display = "none";
-				document.querySelector("#guessdiv p").innerHTML = "There is no keyword for this clue. We will be waiting for you at this location.";
-			}
-			else
-				showClue(this.clues[(startidx + numright + 1) % 4]);
 			this.gpPlayer.addNumRight();
-			showMessage("");
+			document.querySelector("#nextdiv button").disabled = false;
+			document.querySelector("#guessdiv button").disabled = true;
+			showMessage("Great Job. Click the button below for the next clue.");
 		}
+	},
+	
+	// Displays the next clue. Ensures the last clue is last for every player regardless of starting index.
+	showNextClue: function(){
+		let startidx = this.gpPlayer.getStartIdx();
+		let numright = this.gpPlayer.getNumRight();
+		clearInput();
+		document.querySelector("#nextdiv button").disabled = true;
+		document.querySelector("#guessdiv button").disabled = false;
+		if (numright == this.keywords.length){
+			showClue(this.clues[this.clues.length - 1]);
+			document.querySelector("#guessdiv input").style.display = "none";
+			document.querySelector("#guessdiv button").style.display = "none";
+			document.getElementById("nextdiv").style.display = "none";
+			document.querySelector("#guessdiv p").innerHTML = "There is no keyword for this clue. We will be waiting for you at this location.";
+		}
+		else
+			showClue(this.clues[(startidx + numright) % 4]);
+		window.scrollTo(0,0);
+		showMessage("");
 	}
 };
 
